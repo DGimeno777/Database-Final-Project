97ffef4d8963c453530b7dc458ce8e31c3f3f51b
Danny <dgimeno777@gmail.com>
2017-11-28 20:51:57 -0500

    add create ticket query

--- model/ticket_db.php
+++ model/ticket_db.php
@@ -8,5 +8,7 @@ function get_tickets_sold_by_showId($showId) {
 }
 
 function create_ticket($showId, $userId) {
-    
+    global $db;
+    $query = "insert into ticket (Sections, UserID, ShowID) VALUES ('GA','$userId','$showId')";
+    $db->exec($query);
 }
