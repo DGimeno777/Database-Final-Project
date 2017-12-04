681ed20ac3e8bb5d3a15d0c47926ba551678006d
Jack Wilkin <wilkin.j@husky.neu.edu>
2017-12-04 16:30:21 -0500

    added artist profile and fixed artist qs

--- model/artist_db.php
+++ model/artist_db.php
@@ -1,13 +1,13 @@
 <?php
 function remove_song_from_set_list($songID, $performanceID){
     global $db;
-    $query = "Call remove_song_from_set_list($songID, $performanceID);";
+    $query = "Call remove_song_from_set_list('$songID', '$performanceID');";
     $db->exec($query);
 }
 
 function add_song_to_set_list($songID, $performanceID, $songOrder){
     global $db;
-    $query = "Call add_song_to_set_list($songID, $performanceID, $songOrder);";
+    $query = "Call add_song_to_set_list('$songID', '$performanceID', '$songOrder');";
     $db->exec($query);
 }
 
