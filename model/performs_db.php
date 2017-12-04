6f02de2970746a9f0ac9db70ddf79e9cd3f2bd02
Lindsay Hauser <lindsay@Lindsays-MacBook-Pro.local>
2017-12-04 16:26:04 -0500

    performs queries

--- model/performs_db.php
+++ model/performs_db.php
@@ -1,15 +1,14 @@
 <?php
 function get_headliner($showid) {
     global $db;
-    $query = "select ArtistName from Shows join Performs using (ShowID) join Artist using (ArtistID) where Shows.ShowID = $showid and Headline = 'Headline';";
+    $query = "select * from Shows join Performs using (ShowID) join Artist using (ArtistID) where Shows.ShowID = $showid and Headline = 'Headline';";
     $query = $db->query($query);
     return $query;
 }
 
 function get_opener($showid) {
     global $db;
-    $query = "select ArtistName from Shows join Performs using (ShowID) join Artist using (ArtistID) where Shows.ShowID = $showid and Headline = 'Opener';";
+    $query = "select * from Shows join Performs using (ShowID) join Artist using (ArtistID) where Shows.ShowID = $showid and Headline = 'Opener';";
     $query = $db->query($query);
     return $query;
-}
 }
