fdf1a29330bfe3a5b8e812a15e6dad8a67a90d8c
Danny <dgimeno777@gmail.com>
2017-12-04 14:48:28 -0500

    removed duplicated db methods

--- model/artist_db.php
+++ model/artist_db.php
@@ -11,13 +11,6 @@ function add_song_to_set_list($songID, $performanceID, $songOrder){
     $db->exec($query);
 }
 
-//Not Done
-function find_close_venues($venueName){
-    global $db;
-    $query = "select * from Venue where VenueName = '$venueName'";
-    $db->exec($query);
-}
-
 //Gets all the artists in the database
 function get_all_artists(){
     global $db;
