c9ccdb9cf3f21c1677d3732e879485a6ee72b973
Lindsay Hauser <lindsay@Lindsays-MacBook-Pro.local>
2017-12-06 14:44:10 -0500

    Added venue use cases

--- model/venue_db.php
+++ model/venue_db.php
@@ -27,3 +27,27 @@ function get_venue_by_user_id($userID){
     $query = $db->query($query);
     return $query;
 }
+
+//Use Case 5 (Venue adds an artist to a show)
+//this has a procedure
+function add_artist_to_show($artist, $show, $headline){
+    global $db;
+    $query = "insert into Performs (ArtistID, ShowID, Headline) values ('$artist', '$show', '$headline');";
+    $db->exec($query);
+}
+
+//Use Case 6 (A venue wants to remove a list of artists from a specific show)
+function remove_artist_from_show($artist, $show){
+    global $db;
+    $query = "delete from Performs where ArtistID = '$artist' and ShowID = '$show';";
+    $db->exec($query);
+}
+
+//Use Case 11 (a venue wants to see artists similar to the given one to find a headliner/opener for a show
+function find_similar_artists($artistID){
+    global $db;
+    $query = "select * from Artist where Genre = (select Genre from Artist where ArtistID = '$artistID')";
+    $query = $db->query($query);
+    return $query;
+}
+
